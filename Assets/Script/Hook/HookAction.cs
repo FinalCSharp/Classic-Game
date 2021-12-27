439dcf0599a9c6d7721839fa6e931c6adf4fb59c
Kyl23 <cklkailiang@gmail.com>
2021-12-27 18:14:33 +0800

    fix: hook orbit ,feat: prepare a lot of script haven't used yet, count down & count score (money)

--- Assets/Script/Hook/HookAction.cs
+++ Assets/Script/Hook/HookAction.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class HookAction : MonoBehaviour
 {
-    //fix:1. the rotation should be around a point.
-    //fix:2. the object mass?
-    //fix:3. the y direction is wrong.
     private bool MOVE = false, shouldUp = false;
     private bool R_direction = true;
     public float originRotationPoint = 64.199f, ableRationDegree = 70, hookSpeed = 150,rotateSpeed = 200;
+    float duplicatedHookSpeed;
     float originX, originY;
-    float totalX, totalY, x;
+    float totalX, totalY, degree;
+    bool isCathed = false, isQpack = false;
+    public float[] qPackSpeedTable;
+    public float[] speedTable;
+    GameObject soundPlayer;
     // Start is called before the first frame update
     void Start()
     {
+        soundPlayer = GameObject.Find("SoundPlayer");
+        duplicatedHookSpeed = hookSpeed;
         totalX = totalY = 0; // init
         originX = transform.localPosition.x;
         originY = transform.localPosition.y;
@@ -36,26 +40,48 @@ public class HookAction : MonoBehaviour
             totalX = totalY = 0;
             Down();
             MOVE = true;
+            isQpack = isCathed = false;
         }
         else if (!MOVE) rotating();
         else Down();
     }
+    int getItemCode()
+    {
+        int i; // counter
+        if (isQpack)
+        {
+            for (i = 0; i < qPackSpeedTable.Length; i++)
+                if (qPackSpeedTable[i] == hookSpeed)
+                    return i + speedTable.Length;
+        }
+        else
+        {
+            for (i = 0; i < speedTable.Length; i++)
+                if (speedTable[i] == hookSpeed)
+                    return i;
+        }
+        return -1;//error
+    }
     void Up()
     {
+        float x = -hookSpeed / (float)System.Math.Cos(degree) * Time.deltaTime;
         transform.Translate(-x, hookSpeed * Time.deltaTime , 0);
-        totalX -= x;
-        totalY += hookSpeed * Time.deltaTime;
-        if (totalY >= 0) {
+        if (transform.localPosition.y >= originY) {
             transform.localPosition = new Vector2(originX, originY);
             shouldUp = false;
             MOVE = false;
+            int code = getItemCode();
+            GameObject.Find("Score").SendMessage("pickedItem", code);
+            if(code != -1)
+                freeHook();
+            revertHookSpeed();
         }
     }
     void Down() {
         //moving toward the angel chosen.
-        float degree = originRotationPoint - transform.rotation.z;
+        degree = originRotationPoint - transform.rotation.z;
         //follow the degree to fly ~~~
-        x = -hookSpeed / (float)System.Math.Cos(degree) * Time.deltaTime;
+        float x = -hookSpeed / (float)System.Math.Cos(degree) * Time.deltaTime;
         transform.Translate(x, -hookSpeed * Time.deltaTime, 0);
         totalY -= hookSpeed * Time.deltaTime;
         totalX = totalY / (float)System.Math.Cos(degree);
@@ -78,14 +104,50 @@ public class HookAction : MonoBehaviour
         if (R_direction) transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
         else transform.Rotate(0, 0 , -rotateSpeed * Time.deltaTime);
     }
-    
+    void freeHook()
+    {
+        Destroy(transform.GetChild(0).gameObject);
+        //sound effect here
+        //soundPlayer.SendMessage("playSoundEffect")
+    }
+    void revertHookSpeed()
+    {
+        hookSpeed = duplicatedHookSpeed;
+    }
+    System.Random rdm = new System.Random();
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        /*
-        if (collision.gameObject.tag == "Bgold") {
-            //Back();//Q: game object mass?
+        Transform clsn = collision.gameObject.transform;
+        if (isCathed) return;
+        clsn.parent = transform;
+        clsn.localPosition = new Vector2(0, 0);
+        shouldUp = true;
+        isCathed = true;
+        switch (collision.gameObject.name)
+        {
+            //sound effect here
+            case "CQpack(Clone)": // 問號包
+                isQpack = true;
+                hookSpeed = qPackSpeedTable[rdm.Next() % (qPackSpeedTable.Length)];
+                break;
+            case "CBgold(Clone)": // big gold
+                hookSpeed = speedTable[0];
+                break;
+            case "CMgold(Clone)": // medium gold
+                hookSpeed = speedTable[1];
+                break;
+            case "CSgold(Clone)": // small gold
+                hookSpeed = speedTable[2];
+                break;
+            case "CBstone2(Clone)": // big stone
+                hookSpeed = speedTable[3];
+                break;
+            case "CMstone2(Clone)": // medium stone
+                hookSpeed = speedTable[4];
+                break;
+            case "CSstone2(Clone)": // small stone
+                hookSpeed = speedTable[5];
+                break;
         }
-        */
-        Debug.Log(collision.gameObject.name);
     }
 }
