112acdb2bdd273e900dbda26e117ad29b559afd8
Otuslettia <95861205+Otuslettia@users.noreply.github.com>
2022-01-04 01:50:01 +0800

    Remove Script
    Remove MapSpawner1

--- Assets/Script/BoomerMatrixSpawn.cs
+++ Assets/Script/BoomerMatrixSpawn.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapSpawner1 : MonoBehaviour
+public class BoomerMatrixSpawn : MonoBehaviour
 {
+    // 0: space
+    // 0, 14 can be Destory
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void SpawnMatrix() {
+
     }
 }
