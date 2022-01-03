5dab92fd3fa1a369377e4c8991fd7b49223da955
0900116xxx <qingyutin@gmail.com>
2022-01-04 00:21:58 +0800

    ...

--- Assets/Script/products_ClickRemove.cs
+++ Assets/Script/products_ClickRemove.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 
-public class products_ClickRemove : MonoBehaviour
+public class products_ClickRemove : MonoBehaviour , IPointerClickHandler
 {
     GameObject button;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+            Destroy(button);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +21,11 @@ public class products_ClickRemove : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if (Input.GetMouseButton(0))
         {
             Destroy(button);
         }
-    }
+    }*/
 }
