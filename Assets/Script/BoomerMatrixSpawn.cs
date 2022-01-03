7bd90adfb5a38c0ddffe7712d2ed46056e0c6314
Otuslettia <95861205+Otuslettia@users.noreply.github.com>
2022-01-04 03:24:12 +0800

    Fix Bugs in Random Matrix Spawner.
    I think I should not use Random to construct the Map Matrix. Whatever.

--- Assets/Script/BoomerMatrixSpawn.cs
+++ Assets/Script/BoomerMatrixSpawn.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoomerMatrixSpawn : MonoBehaviour
 {
-    static int Width = 10, Height = 15;
+    static int Width = 15, Height = 10;
     public int[,] Matrix = new int[Width, Height];
     static int Space = 0, Unbreak = -2, Breakable = -1;
     // 0: space
@@ -18,11 +18,14 @@ public class BoomerMatrixSpawn : MonoBehaviour
     void Update() { }
     public void SpawnMatrix()
     {
-        for (int i = 0; i < Width; i++)
+        for (int i = 1; i < Width; i++)
         {
             int Item = Random.Range(0, 1);
             if (Item == 0) Matrix[i, 0] = Space;
             else Matrix[i, 0] = Breakable;
+            Item = Random.Range(0, 1);
+            if (Item == 0) Matrix[i, Height-1] = Space;
+            else Matrix[i, Height-1] = Breakable;
         }
         for (int i = 1; i < Width - 1; i++)
         {
@@ -33,6 +36,14 @@ public class BoomerMatrixSpawn : MonoBehaviour
                 else if (Item == 1) Matrix[i, j] = Unbreak;
                 else Matrix[i, j] = Breakable;
             }
+        }
+        for (int i = 1; i < Height; i++) {
+            int Item = Random.Range(0, 1);
+            if (Item == 0) Matrix[0, i] = Space;
+            else Matrix[0, i] = Breakable;
+            Item = Random.Range(0, 1);
+            if (Item == 0) Matrix[Width-1,i] = Space;
+            else Matrix[Width - 1, i] = Breakable;
         }
         Matrix[0, 0] = Matrix[1, 0] = Matrix[0, 1] =
         Matrix[Width - 1, Height - 1] = Matrix[Width - 2, Height - 1] = Matrix[Width - 1, Height - 2] =
