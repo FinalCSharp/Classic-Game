103473a9a827ae4535d8e44d816d83c970bc9f45
Otuslettia <95861205+Otuslettia@users.noreply.github.com>
2022-01-04 10:14:36 +0800

    BoomerMatrixSpawner_Update
    Add Extra method for Destroy Block

--- Assets/Script/BoomerMatrixSpawn.cs
+++ Assets/Script/BoomerMatrixSpawn.cs
@@ -4,50 +4,54 @@ using UnityEngine;
 
 public class BoomerMatrixSpawn : MonoBehaviour
 {
-    static int Width = 15, Height = 10;
     public int[,] Matrix = new int[Width, Height];
-    static int Space = 0, Unbreak = -2, Breakable = -1;
-    // 0: space
+    private static int Width = 15, Height = 10;
+    private static int Space = 0, Breakable = 1, Unbreakable = 2;
+    private static int[] P_x = { 0, 0, Width - 1, Width - 1 }, P_y = {0,Height-1,0,Height-1 };
     // 0, 14 line can be Destory
     // Start is called before the first frame update
     void Start()
     {
+        Random.seed = System.Guid.NewGuid().GetHashCode();
         SpawnMatrix();
     }
     // Update is called once per frame
-    void Update() { }
+    //void Update() { }
+    //
+    public void DestroyBlock(int x, int y)
+    {//Distroy Blocks:(int location(x, y))//Just Use Trigger to Do It? Anyway.
+        if (Matrix[x, y] == Breakable)
+        {
+            Matrix[x, y] = Space;
+            //call the function in DisplayScript to update the map;
+        }
+    }
     public void SpawnMatrix()
     {
-        for (int i = 1; i < Width; i++)
-        {
-            int Item = Random.Range(0, 2);
-            if (Item == 0) Matrix[i, 0] = Space;
-            else Matrix[i, 0] = Breakable;
-            Item = Random.Range(0, 2);
-            if (Item == 0) Matrix[i, Height-1] = Space;
-            else Matrix[i, Height-1] = Breakable;
-        }
-        for (int i = 1; i < Width - 1; i++)
-        {
-            for (int j = 1; j < Height - 1; j++)
-            {
+        for (int i = 0; i < Width; i++) {
+            for (int j = 0; j < Height; j++) {
                 int Item = Random.Range(0, 3);
-                if (Item == 0) Matrix[i, j] = Space;
-                else if (Item == 1) Matrix[i, j] = Unbreak;
-                else Matrix[i, j] = Breakable;
+                Matrix[i, j] = Item;
             }
         }
-        for (int i = 1; i < Height; i++) {
-            int Item = Random.Range(0, 2);
-            if (Item == 0) Matrix[0, i] = Space;
-            else Matrix[0, i] = Breakable;
-            Item = Random.Range(0, 2);
-            if (Item == 0) Matrix[Width-1,i] = Space;
-            else Matrix[Width - 1, i] = Breakable;
+        //Make Sure the Way to Other Player Exist.
+        for (int i = 0; i < Width; i++) {
+            if (Matrix[i, 0] == Unbreakable) Matrix[i, 0] = Breakable;
+            if (Matrix[i, Height - 1] == Unbreakable) Matrix[i, Height-1] = Breakable;
         }
-        Matrix[0, 0] = Matrix[1, 0] = Matrix[0, 1] =
-        Matrix[Width - 1, Height - 1] = Matrix[Width - 2, Height - 1] = Matrix[Width - 1, Height - 2] =
-        Matrix[0, Height - 1] = Matrix[1, Height - 1] = Matrix[0, Height - 2] =
-        Matrix[Width - 1, 0] = Matrix[Width - 1, 1] = Matrix[Width - 2, 0] = Space;
+        for (int i = 0; i < Height; i++)
+        {
+            if (Matrix[0, i] == Unbreakable) Matrix[0, i] = Breakable;
+            if (Matrix[Width - 1, i] == Unbreakable) Matrix[Width-1, i] = Breakable;
+        }
+        //Setting Player Start Point.
+        for (int i = 0; i < 4; i++)
+        {
+            Matrix[P_x[i], P_y[i]] = Space;
+            if (P_x[i] == 0) Matrix[P_x[i] + 1, P_y[i]] = Space;
+            else Matrix[P_x[i] - 1, P_y[i]] = Space;
+            if (P_y[i] == 0) Matrix[P_x[i], P_y[i] + 1] = Space;
+            else Matrix[P_x[i], P_y[i] - 1] = Space;
+        }
     }
 }
